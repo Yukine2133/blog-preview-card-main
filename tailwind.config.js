3d296c9baf86558fb109d49f2ce89eba3699a573
Yukine213 <kondratenkov1109@gmail.com>
2024-01-03 19:56:11 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        black: "hsl(0, 0%, 7%)",
+      },
+    },
+  },
+  plugins: [],
+};
